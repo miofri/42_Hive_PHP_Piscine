2e35b0a8a05d82320f0c7d7b0af40aea261ddd9b
root <root@Selvi.localdomain>
2022-09-19 16:58:50 +0300

    - ex00:     - added `array_key_exists` check for Username, Password, and for the initial php code to prevent undefined warning as if login or passwd is not in `$_GET`, the variables would be undefined. - ex01:     - added the ability to create multiple user by appending new `login` and `passwd` to the unserialized `passwd` - ex02:     - no modification necessary - ex03     - Changed `$_SESSION[$loggued_on_user]` to `$_SESSION[”loggued_on_user”]` as the former is NOT a session variable and instead is an undefined variable.

--- d04/ex03/auth.php
+++ d04/ex03/auth.php
@@ -3,8 +3,8 @@
 	{
 		if ($login && $passwd)
 		{
-			$us = unserialize(file_get_contents("../private/passwd"));
-			foreach ($us as $key => $value)
+			$unserialized = unserialize(file_get_contents("../private/passwd"));
+			foreach ($unserialized as $key => $value)
 			{
 				if ($value["login"] === $login && $value["passwd"] === hash("whirlpool", $passwd) && $passwd)
 					return(TRUE);
